0c150de1494f4136261abd77fefff0c287d8de26
Nicolas <nicolagfalabella@gmail.com>
2026-06-14 18:57:43 -0300

    Day 1 : add first go program

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hola mundo")
+	fmt.Println("Estoy aprendiendo Go para backend")
+	fmt.Println("Mi objetivo es construir SocialWallet API")
+}
