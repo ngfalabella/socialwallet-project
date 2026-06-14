069f57ff782581386f523d0145ef35434b6100b4
Nicolas <nicolagfalabella@gmail.com>
2026-06-14 19:57:47 -0300

    Day 1:practice variables and formatted output

--- main.go
+++ main.go
@@ -3,23 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola soy Gabriel")
-	fmt.Println("Estoy aprendiendo Go")
-	fmt.Println("Quiero trabajar como Golang Backend Developer")
-	fmt.Println("Hoy completé mi primer programa")
 
-	// Ejercicio 2 
-	fmt.Println("SocialWallet API")
-	fmt.Println("Backend con Go")
-	fmt.Println("Estado: aprendiendo fundamentos")
-	fmt.Println("Día: 1")
+	var senderName string = "Gabriel"
+	var receiverName string = "Ana"
+	amountInCents := 2500
+	senderBalanceInCents := 10000
+	receiverBalanceInCents := 2500
+	operationID := "TRX-001"
+	var isSuccessful bool
+	isSuccessful = true
 
+	senderBalanceAfterInCents := senderBalanceInCents - amountInCents
+	receiverBalanceAfterInCents := receiverBalanceInCents + amountInCents
 
-	//Ejercicio 3
-	// 1 - undefined: fmt
-	// 2 - function main is undeclared in the main package
-	// 3 - package command-line-arguments is not a main package
-	// 4 - syntax error
-	// 5 - : syntax error: unexpected EOF, expected
+	var summary string = fmt.Sprintf("Resumen: %s transfirio %d centavos a %s", senderName, amountInCents, receiverName)
 
+	fmt.Println("=== SocialWallet Transfer ===")
+	fmt.Println("Operación:", operationID)
+	fmt.Println("Origen:", senderName)
+	fmt.Println("Destino:", receiverName)
+	fmt.Println("Monto:", amountInCents, "centavos")
+	fmt.Printf("Saldo origen antes: %d centavos\n", senderBalanceInCents)
+	fmt.Println("Saldo destino antes:", receiverBalanceInCents, "centavos")
+	fmt.Println("Transferencia exitosa:", isSuccessful)
+	fmt.Println(summary)
+	fmt.Printf("Saldo origen después:%d centavos \n", senderBalanceAfterInCents)
+	fmt.Printf("Saldo destino después:%d centavos \n", receiverBalanceAfterInCents)
 }
