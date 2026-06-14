a988cef20a0d6f717493296268647225acaf2c35
Nicolas <nicolagfalabella@gmail.com>
2026-06-14 19:14:22 -0300

    Day 1 : Practice hello word fundamentals

--- main.go
+++ main.go
@@ -3,7 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
-	fmt.Println("Estoy aprendiendo Go para backend")
-	fmt.Println("Mi objetivo es construir SocialWallet API")
+	fmt.Println("Hola soy Gabriel")
+	fmt.Println("Estoy aprendiendo Go")
+	fmt.Println("Quiero trabajar como Golang Backend Developer")
+	fmt.Println("Hoy completé mi primer programa")
+
+	// Ejercicio 2 
+	fmt.Println("SocialWallet API")
+	fmt.Println("Backend con Go")
+	fmt.Println("Estado: aprendiendo fundamentos")
+	fmt.Println("Día: 1")
+
+
+	//Ejercicio 3
+	// 1 - undefined: fmt
+	// 2 - function main is undeclared in the main package
+	// 3 - package command-line-arguments is not a main package
+	// 4 - syntax error
+	// 5 - : syntax error: unexpected EOF, expected
+
 }
