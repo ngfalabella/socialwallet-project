db8de7053e7179d15f85c5715a6135cd591cc2f2
Nicolas <nicolagfalabella@gmail.com>
2026-06-16 21:06:27 -0300

    Day 2 : Types

--- main.go
+++ main.go
@@ -4,29 +4,16 @@ import "fmt"
 
 func main() {
 
-	var senderName string = "Gabriel"
-	var receiverName string = "Ana"
-	amountInCents := 2500
-	senderBalanceInCents := 10000
-	receiverBalanceInCents := 2500
-	operationID := "TRX-001"
-	var isSuccessful bool
-	isSuccessful = true
+	var balanceInCents int = 15000
+	var trasnferAmountInCents int = 2500
+	var balanceAfterTransferInCents int = balanceInCents - trasnferAmountInCents
 
-	senderBalanceAfterInCents := senderBalanceInCents - amountInCents
-	receiverBalanceAfterInCents := receiverBalanceInCents + amountInCents
 
-	var summary string = fmt.Sprintf("Resumen: %s transfirio %d centavos a %s", senderName, amountInCents, receiverName)
+	var hasEnoughBalance bool
 
-	fmt.Println("=== SocialWallet Transfer ===")
-	fmt.Println("Operación:", operationID)
-	fmt.Println("Origen:", senderName)
-	fmt.Println("Destino:", receiverName)
-	fmt.Println("Monto:", amountInCents, "centavos")
-	fmt.Printf("Saldo origen antes: %d centavos\n", senderBalanceInCents)
-	fmt.Println("Saldo destino antes:", receiverBalanceInCents, "centavos")
-	fmt.Println("Transferencia exitosa:", isSuccessful)
-	fmt.Println(summary)
-	fmt.Printf("Saldo origen después:%d centavos \n", senderBalanceAfterInCents)
-	fmt.Printf("Saldo destino después:%d centavos \n", receiverBalanceAfterInCents)
+	hasEnoughBalance = balanceInCents >= trasnferAmountInCents
+
+	fmt.Printf("Saldo suficiente:%t\n", hasEnoughBalance)
+	fmt.Printf("Saldo :%d\n", balanceAfterTransferInCents)
+
 }
