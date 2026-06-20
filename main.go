23702c05949ef8eaf3424aca3241a5f304c8a2d6
Nicolas <nicolagfalabella@gmail.com>
2026-06-20 14:58:59 -0300

    Funciones

--- main.go
+++ main.go
@@ -2,18 +2,51 @@ package main
 
 import "fmt"
 
+const operationWithdraw = "withdraw"
+const operationDeposit = "deposit"
+const operationTransfer = "transfer"
+
+func processWalletOperation(balanceInCents int, amountInCents int, operation string) (int, bool) {
+
+	if amountInCents <= 0 {
+		return balanceInCents, false
+	}
+
+	if amountInCents > balanceInCents {
+		return balanceInCents, false
+	}
+
+	switch operation {
+	case operationWithdraw:
+		return balanceInCents - amountInCents, true
+
+	case operationDeposit:
+		return balanceInCents + amountInCents, true
+
+	case operationTransfer:
+		return balanceInCents - amountInCents, true
+
+	default:
+		return balanceInCents, false
+
+	}
+
+}
+
 func main() {
 
-	var balanceInCents int = 15000
-	var trasnferAmountInCents int = 2500
-	var balanceAfterTransferInCents int = balanceInCents - trasnferAmountInCents
+	userBalanceTest := 10000
+	userAmounTest := 2500
+	userOperationTest := operationDeposit
 
+	result, ok := processWalletOperation(userBalanceTest, userAmounTest, userOperationTest)
 
-	var hasEnoughBalance bool
+	if !ok {
+		fmt.Println("Ocurrio un error al procesar")
+		fmt.Println("Saldo actual : ", result)
+		return
+	}
 
-	hasEnoughBalance = balanceInCents >= trasnferAmountInCents
-
-	fmt.Printf("Saldo suficiente:%t\n", hasEnoughBalance)
-	fmt.Printf("Saldo :%d\n", balanceAfterTransferInCents)
+	fmt.Println("Saldo actual : ", result)
 
 }
