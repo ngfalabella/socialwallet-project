4ddc13cb88a5042b358e0426a23b1e59a39796bf
Nicolas <nicolagfalabella@gmail.com>
2026-07-02 21:18:13 -0300

    Testing dia 2

--- domain/wallet_test.go
+++ domain/wallet_test.go
@@ -1,48 +1,94 @@
-package domain 
+package domain
 
-import ( 
-	"testing"
-	"errors"
-)
+import "testing"
 
-func TestDepositNegativeAmount(t *testing.T) {
-	wallet ,err := NewWallet(1 , CurrencyARS)
+func TestValidOperation(t *testing.T) {
+	newWalletCreated , err := NewWallet(1 , CurrencyARS)
+
 	if err != nil {
-		t.Fatalf("no se pudo crear la wallet : %v" , err)
+		t.Fatalf("Ocurrio un error al crear wallet : %v" , 
+		err ,
+		)
 	}
 
-	err = wallet.Deposit(500)
+	err = newWalletCreated.Deposit(1000)
+
 	if err != nil {
-		t.Fatalf("el deposito devolvio un error inesperado : %v" , err)
+		t.Fatalf("Ocurrio un error al Depositar : %v" , err)
 	}
 
-	if wallet.WalletBalance != 500 {
-		t.Fatalf("Saldo esperado : 500 , saldo obtenido :%d" ,wallet.WalletBalance)
+	if newWalletCreated.WalletBalance != 1000 {
+		t.Fatalf("Se esperaba saldo de $500 y se obtuvo $%d" , newWalletCreated.WalletBalance)
 	}
 
+	err = newWalletCreated.Withdraw(300)
+
+	if err != nil {
+		t.Fatalf("Ocurrio un error al hacer la extraccion : %v" , err)
+	}
+
+	if newWalletCreated.WalletBalance != 700 {
+		t.Fatalf("Se esperaba un saldo de 700 y se obtuvo :%v" , newWalletCreated.WalletBalance)
+	}
+
+	err = newWalletCreated.Deposit(200)
+
+	if err != nil {
+		t.Fatalf("Ocurrio un fallo al hacer el segundo deposito : %v" , err)
+	}
+
+	if newWalletCreated.WalletBalance != 900 {
+		t.Fatalf("Se esperaba un saldo de $900 y se obtuvo $%d" , newWalletCreated.WalletBalance)
+	}
 }
 
-func TestWalletValidCurrency( t *testing.T) {
-	wallet,err := NewWallet( 2 , CurrencyARS)
-	 
+func TestExtractAllMount( t *testing.T) {
+	newWalletCreated , err := NewWallet(2 , CurrencyARS)
+
 	if err != nil {
-		t.Fatalf("No se pudo crear wallet  :%v" , err)
+		t.Fatalf("Ocurrio un error al crear wallet : %v" , err)
 	}
 
-	err = wallet.Deposit(-500)
+	err = newWalletCreated.Deposit(700)
 
-	if err == nil {
-		t.Fatalf("Se esperaba un error pero se obtuvo nil")
+	if err != nil {
+		t.Fatalf("Ocurrio un error al hacer deposito : %v" , err)
 	}
 
-	if !errors.Is(err,InvalidMountWallet) {
-		t.Fatalf("Se esperaba invaiidMounwaller, pero se obtuvo %v" ,err)
+	if newWalletCreated.WalletBalance != 700 {
+		t.Fatalf("Se esperaba tener 700 y se obtuvo %d" , newWalletCreated.WalletBalance)
 	}
 
-	if wallet.WalletBalance != 0 {
-	t.Fatalf(
-		"el saldo debía permanecer en 0, pero quedó en %d",
-		wallet.WalletBalance,
-	)
+	err = newWalletCreated.Withdraw(700)
+
+	if err != nil {
+		t.Fatalf("Ocurrio un error al hacer la extraccion %v" , err)
+	}
+
+	if newWalletCreated.WalletBalance != 0 {
+		t.Fatalf("El cliente no deberia tener saldo y termino teniendo : %d" , newWalletCreated.WalletBalance)
+	}
 }
+
+func TestSaldoInsuficiente ( t *testing.T) {
+	newWalletCreated , err := NewWallet(3 , CurrencyARS)
+
+	if err != nil {
+		t.Fatalf("Error al crear wallet : %v" , err)
+	}
+
+	err = newWalletCreated.Deposit(400)
+
+	if err != nil {
+		t.Fatalf("Ocurrio un error al depositar : %v" ,err)
+	}
+
+	if newWalletCreated.WalletBalance != 400 {
+		t.Fatalf("Se esperaba tener $400 y hay  %d " , newWalletCreated.WalletBalance)
+	}
+
+	err =newWalletCreated.Withdraw(500)
+
+	e
+
 }
